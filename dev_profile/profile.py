3d8c9cb0abc79f32d794939f2d67f4b90b213379
Alexis de Lattre <alexis.delattre@akretion.com>
2019-02-01 10:22:51 +0100

    Port to v12

--- dev_profile/profile.py
+++ dev_profile/profile.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2013-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2013-2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api
@@ -16,11 +16,11 @@ class AccountJournal(models.Model):
     update_posted = fields.Boolean(default=True)
 
 
-class BaseLanguageExport(models.TransientModel):
-    _inherit = 'base.language.export'
+#class BaseLanguageExport(models.TransientModel):
+    #_inherit = 'base.language.export'
 
     # Default format for language files = format used by OpenERP modules
-    format = fields.Selection(default='po')
+    #format = fields.Selection(default='po')
 
 
 class BaseLanguageInstall(models.TransientModel):
